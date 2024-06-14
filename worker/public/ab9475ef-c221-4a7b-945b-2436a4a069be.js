fd420b838b40cc15264f9e76933b33bb32e9ed31
CODECZERO <as8857981@gmail.com>
2024-06-15 00:00:11 +0530

    working frontend limit websocket and work on docker file, worker

--- worker/public/ab9475ef-c221-4a7b-945b-2436a4a069be.js
+++
@@ -1,1 +0,0 @@
-conosole.log("Hello Wolrd")
--- worker/src/index.js
+++ worker/src/index.js
@@ -4,7 +4,8 @@ import { v4 as uuidv4 } from "uuid";
 import fs from "fs";
 import { exec } from "child_process";
 import { error } from "console";
-
+import {config} from "dotenv";
+config()
 
 const Configs = {
     python: {
@@ -27,16 +28,19 @@ const Configs = {
     }
 };
 
+const REDIS_HOST = process.env.REDIS_HOST || 'redis';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
 
 const StartServer = async () => {
-    const client = createClient().on('error', err => console.log(`Error connecting to Redis: ${err}`));;
+    const client = createClient({ url: `redis://${REDIS_HOST}:${REDIS_PORT}` }).on('error', err => console.log(`Error connecting to Redis: ${err}`));;
     await client.connect();
     console.log('Connected to Redis');
 
 
     while (true) {
         try {
-            
+
             await new Promise((resolve) => {
                 setTimeout(resolve, 1000);
             });
@@ -46,7 +50,7 @@ const StartServer = async () => {
 
 
             if (!language || !code) {
-                await client.publish(`${process.env.PUBNAME ||'response_'}`, JSON.stringify({
+                await client.publish(`${process.env.PUBNAME || 'response_'}`, JSON.stringify({
                     jobID,
                     userID,
                     error: 'Language or code is not provided'
@@ -56,7 +60,7 @@ const StartServer = async () => {
 
             const config = Configs[language];
             if (!config) {
-                await client.publish(`${process.env.PUBNAME ||'response_'}`, JSON.stringify({
+                await client.publish(`${process.env.PUBNAME || 'response_'}`, JSON.stringify({
                     jobID,
                     userID,
                     error: 'Unsupported language'
@@ -70,44 +74,55 @@ const StartServer = async () => {
             fs.writeFileSync(filePath, code);
 
             const compileCommand = config.execute ? `${config.command} ${filePath}` : `${config.command} ${filePath}`;
+
             exec(compileCommand, (compileError, stdout, stderr) => {
                 if (compileError || stderr) {
-                    client.publish(`${process.env.PUBNAME ||'response_'}`, JSON.stringify({
+                    client.publish(`${process.env.PUBNAME || 'response_'}`, JSON.stringify({
                         jobID,
                         userID,
                         error: compileError ? compileError.message : stderr
                     }));
-                    fs.unlinkSync(filePath); // Clean up file
+                    fs.unlinkSync(filePath);
                     return;
                 }
-
                 if (config.execute) {
                     exec(config.execute, (runError, runStdout, runStderr) => {
-                        client.publish(`${process.env.PUBNAME ||'response_'}`, JSON.stringify({
-                            jobID,
-                            userID,
-                            output: runError ? runStderr : runStdout,
-                            error: runError ? runError.message : null
-                        }));
-                        console.log(runError);
-                        fs.unlinkSync(filePath); // Clean up file
+                        if (runStdout) {
+                            client.publish(`${process.env.PUBNAME || 'response_'}`, JSON.stringify({
+                                jobID,
+                                userID,
+                                output: runStdout,
+                            }));
+                            fs.unlinkSync(filePath);
+                            fs.unlinkSync("a.out");
+                        }
+                        else {
+                            client.publish(`${process.env.PUBNAME || 'response_'}`, JSON.stringify({
+                                jobID,
+                                userID,
+                                error: runError ? runError.message : null
+                            }));
+                            fs.unlinkSync(filePath);
+                            fs.unlinkSync("a.out");
+
+                        }
                     });
                 } else {
-                    const payload=JSON.stringify({
+                    const payload = JSON.stringify({
                         jobID,
                         userID,
                         output: stdout,
                         error: null
                     });
-                    //client.lPush("response_",payload)
-                    if(!payload){
+
+                    if (!payload) {
                         throw error("invalid payload");
                     }
                     client.publish('response_', payload);
-                    console.log(stdout)
                     fs.unlinkSync(filePath); // Clean up file
                 }
             });
+
         } catch (error) {
             console.error(`Unexpected error: ${error.message}`);
         }
